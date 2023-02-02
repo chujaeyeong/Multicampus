5b3f3ab08eb5d5961db3a51141a382530c06a90e
chujaeyeong <123634960+chujaeyeong@users.noreply.github.com>
2023-02-02 17:37:21 +0900

    DAY04 (2023년 2월 2일) 입니다.

---
+++ out2.js
@@ -0,0 +1,14 @@
+function 여행하다() {
+    console.log("1. 여행지를 정한다")
+    console.log("2. 숙소와 교통편을 예약한다")
+    console.log("3. 여행 일정을 정리한다")
+    console.log("4. 사전 예매가 필요한 장소는 예매한다")
+    console.log("5. 짐을 싼다")
+    console.log("6. 여행 당일! 출발하자~")
+ }
+
+ function 쉬다() {
+    console.log("1. 일단 씻고")
+    console.log("2. 누워있는다")
+    console.log("3. 치킨을 시켜먹는다 냠냠")
+ }
