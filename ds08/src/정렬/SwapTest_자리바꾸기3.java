e5261817d290a7572573f81f4447fe612075bee9
Chu Jae Yeong <123634960+chujaeyeong@users.noreply.github.com>
2023-04-19 17:52:54 +0900

    DS/Algorithm DAY 08 (2023.04.19)
    04/19 ▶︎ 스택, 큐, 자료구조 복습 / 정렬 알고리즘 개념 학습 및 문제 풀이 / 스프링 AOP / 스프링 환경설정 복습

---
+++ ds08/src/정렬/SwapTest_자리바꾸기3.java
@@ -0,0 +1,28 @@
+package 정렬;
+
+import java.util.Arrays;
+
+public class SwapTest_자리바꾸기3 {
+
+	public static void main(String[] args) {
+		String[] str = {"햄버거", "샌드위치", "커피"};
+		print(str);
+		swap(str, 0, 1); //햄버거, 샌드위치 바꾸어보세요.
+		print(str);
+	}
+
+	public static void swap(String[] num, int x, int y) {
+		String temp = num[y];
+		num[y] = num[x];
+		num[x] = temp;
+	}
+
+	public static void print(int[] num) {
+		System.out.println(Arrays.toString(num));
+	}
+
+	public static void print(String[] str) {
+		System.out.println(Arrays.toString(str));
+	}
+
+}
